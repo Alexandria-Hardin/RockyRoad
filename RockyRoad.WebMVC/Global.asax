367bdf5bc041ae3ba8183d30da9c9c70178143d6
Alexandria Hardin <aphardin101@gmail.com>
2021-08-16 12:02:36 -0400

    Add project files.

---
+++ RockyRoad.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RockyRoad.WebMVC.MvcApplication" Language="C#" %>
---
+++ RockyRoad.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RockyRoad.WebMVC.Startup))]
+namespace RockyRoad.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
