f8db7b95e4ce9060bc56c101df69a3d67bd54118
Alexandria Hardin <aphardin101@gmail.com>
2021-08-17 11:41:11 -0400

    added favoritepath create and listitem models

---
+++ RockyRoad.Models/FavoritePathCreate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockyRoad.Models
+{
+    public class FavoritePathCreate
+    {
+        public int PathId { get; set; }
+        public int FavoriteId { get; set; }
+    }
+}
---
+++ RockyRoad.Models/FavoritePathListItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockyRoad.Models
+{
+    public class FavoritePathListItem
+    {
+        public int PathId { get; set; }
+        public int FavoriteId { get; set; }
+    }
+}
