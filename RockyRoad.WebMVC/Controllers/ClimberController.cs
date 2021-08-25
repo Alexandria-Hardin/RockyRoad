d27fe9ea37b20ed0231270974a2e7f027bd800a5
Alexandria Hardin <aphardin101@gmail.com>
2021-08-25 13:03:23 -0500

    fixing climber issues

--- RockyRoad.WebMVC/Controllers/ClimberController.cs
+++ RockyRoad.WebMVC/Controllers/ClimberController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using RockyRoad.Data;
 using RockyRoad.Models;
+using RockyRoad.Services;
 
 namespace RockyRoad.WebMVC.Controllers
 {
@@ -46,5 +47,70 @@ namespace RockyRoad.WebMVC.Controllers
 
         }
 
+        public ActionResult Details(string id)
+        {
+            var svc = CreateClimberService();
+            var model = svc.GetClimberById(id);
+
+            return View(model);
+        }
+
+        private ClimberService CreateClimberService()
+        {
+            var service = new ClimberService();
+            return service;
+        }
+
+        public ActionResult Edit(string id)
+        {
+            var service = CreateClimberService();
+            var detail = service.GetClimberById(id);
+            var model =
+                new ClimberEdit
+                {
+                    FirstName = detail.FirstName,
+                    LastName = detail.LastName,
+                    LevelOfExperience = detail.LevelOfExperience,
+                    User = detail.User
+                };
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(string id, ClimberEdit model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            if (model.ClimberId != id)
+            {
+                ModelState.AddModelError("", "Id Mismatch");
+                return View(model);
+            }
+            var service = CreateClimberService();
+            if (service.UpdateClimber(model))
+            {
+                TempData["SaveResult"] = "The climbing profile was updated.";
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError("", "The climbing profile could not be updated.");
+            return View(model);
+        }
+        [ActionName("Delete")]
+        public ActionResult Delete(string id)
+        {
+            var svc = CreateClimberService();
+            var model = svc.GetClimberById(id);
+
+            return View(model);
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeletePost(string id)
+        {
+            var service = CreateClimberService();
+            service.DeleteClimber(id);
+            TempData["SaveResult"] = "Your climbing profile was deleted";
+            return RedirectToAction("Index");
+        }
     }
 }
