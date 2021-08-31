8f65e14ee959fb90e9ccbad812654135bf9b7989
Alexandria Hardin <aphardin101@gmail.com>
2021-08-31 12:42:35 -0500

    Fixing redirects and hrefs

--- RockyRoad.Services/FavoriteService.cs
+++ RockyRoad.Services/FavoriteService.cs
@@ -106,5 +106,31 @@ namespace RockyRoad.Services
                 return ctx.SaveChanges() == 1;
             }
         }
+        //Joining Table
+        public bool CreateFavoritePath(FavoritePathCreate model)
+        {
+            var entity =
+                new FavoritePath()
+                {
+                    PathId = model.PathId,
+                    FavoriteId = model.FavoriteId
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.FavoritePaths.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public bool DeletePathFromFavorites(int pathId, int favoriteId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var foundPath = ctx.FavoritePaths.Single(s => s.PathId == pathId && s.FavoriteId == favoriteId);
+
+                ctx.FavoritePaths.Remove(foundPath);
+                return ctx.SaveChanges() == 1;
+            }
+        }
     }
 }
--- RockyRoad.WebMVC/Controllers/AccountController.cs
+++ RockyRoad.WebMVC/Controllers/AccountController.cs
@@ -164,7 +164,9 @@ namespace RockyRoad.WebMVC.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+
+                    //redirecting to create climber once registered
+                    return RedirectToAction("Create", "Climber");
                 }
                 AddErrors(result);
             }
